69cf28652b4cb5cace96703e7cbccaa0890c0cca
Chang <chang00zhang@gmail.com>
2025-08-24 14:11:51 +0900

    refactor: optimize component structure

--- src/components/DrawCard/DrawCard.jsx
+++ src/components/DrawCard/DrawCard.jsx
@@ -1,24 +1,13 @@
 "use client";
 import { useState } from "react";
-import cards from "@/data/cards.json";
 import { Result } from "../Result/Result";
 import { QuestionInput } from "../QuestionInput/QuestionInput";
+import { DrawButton } from "../DrawButton/DrawButton";
 import style from "./DrawCard.module.css";
 
 export default function DrawCard() {
   const [question, setQuestion] = useState("");
   const [card, setCard] = useState(null);
-
-  function drawCard() {
-    const card = cards[Math.floor(Math.random() * cards.length)];
-    const isReversed = Math.random() < 0.5;
-    const selectedCard = {
-      ...card,
-      meaning: isReversed ? card.reversed : card.upright,
-      isReversed,
-    };
-    setCard(selectedCard);
-  }
 
   return (
     <div className={style.container}>
@@ -26,10 +15,7 @@ export default function DrawCard() {
         value={question}
         onChange={(event) => setQuestion(event.target.value)}
       />
-      <p>請點擊下方按鈕抽牌</p>
-      <button onClick={drawCard} className={style.button}>
-        {card ? "重抽" : "抽牌"}
-      </button>
+      <DrawButton onDraw={(card) => setCard(card)} isDrawn={!!card} />
       {card && (
         <Result
           cardSrc={card.image}
