4b2a05955bc6fa0abfb21109c601b2036d729497
Chang <chang00zhang@gmail.com>
2025-08-31 10:21:42 +0900

    fix: remove duplicated API fetch logic in Result comp

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import { AiExplain } from "@/AiExplain/AiExplain";
 import styles from "./Result.module.css";
 
@@ -10,52 +9,6 @@ export function Result({
   isReversed,
   question,
 }) {
-  const [isLoading, setLoading] = useState(false);
-  const [success, setSuccess] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getCardExplain({
-      question,
-      cardName,
-      cardPosition,
-      cardMeaning,
-    }) {
-      try {
-        // 清除前一次 API 呼叫結果
-        setSuccess(null);
-        setError(null);
-
-        setLoading(true);
-
-        const res = await fetch(
-          `/api/v1/explain?question=${question}&cardName=${cardName}&cardPosition=${cardPosition}&cardMeaning=${cardMeaning}`
-        );
-
-        if (!res.ok) {
-          throw new Error("call API fail");
-        }
-        const data = await res.json();
-
-        setSuccess(data.result);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    // 有卡片跟問題時，才執行 API 呼叫
-    if (cardName && cardMeaning && question) {
-      getCardExplain({
-        question,
-        cardName: cardName,
-        cardPosition: isReversed ? "逆位" : "正位",
-        cardMeaning: cardMeaning,
-      });
-    }
-  }, [question, cardName, cardMeaning, isReversed]);
-
   return (
     <div className={styles.cardArea}>
       <img
