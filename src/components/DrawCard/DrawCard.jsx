64318d5450f70755997fdaefc591953f3083e748
Chang <chang00zhang@gmail.com>
2025-09-07 10:05:34 +0900

    feat: add menu to reset the selected role

--- src/components/DrawCard/DrawCard.jsx
+++ src/components/DrawCard/DrawCard.jsx
@@ -6,16 +6,28 @@ import { DrawButton } from "../DrawButton/DrawButton";
 import style from "./DrawCard.module.css";
 import RoleSelect from "../RoleSelect/RoleSelect";
 import { Title } from "../Title/Title";
+import Menu from "../Menu/Menu";
 
 export default function DrawCard() {
   const [role, setRole] = useState(null);
   const [question, setQuestion] = useState("");
   const [card, setCard] = useState(null);
 
+  function onReset() {
+    setRole(null);
+    setCard(null);
+    setQuestion("");
+  }
+
   return (
     <main className={style.container} tabIndex={-1}>
       {!role && <RoleSelect onSelect={setRole} />}
-      {!!role && <Title>塔羅抽牌工具</Title>}
+      {!!role && (
+        <>
+          <Menu onResetRole={onReset} />
+          <Title>塔羅抽牌工具</Title>
+        </>
+      )}
       {role === "user" && !card && (
         <QuestionInput
           value={question}
